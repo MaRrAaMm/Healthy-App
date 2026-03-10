c5f07df52d508ba2c20c4050d7af68902a7c9fdb
maram <maram@gmail.com>
2026-03-10 04:25:44 +0200

    second

--- src/Modules/userProfile/profile.service.js
+++ src/Modules/userProfile/profile.service.js
@@ -2,6 +2,7 @@ import { UserProfile } from "../../DB/models/userProfile.model.js";
 import { calculateDailyCalories } from "../../utils/health/calculateCalories.js";
 import { calculateMacros } from "../../utils/health/calculateMacros.js";
 import { calculateWeightChangeDuration } from "../../utils/health/calculateWeightDuration.js";
+import { calculateWeightProgress } from "../../utils/health/calculateWeightProgress.js";
 import { User } from "../../DB/models/user.model.js";
 
 
